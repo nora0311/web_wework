9dfda5408c88cf88cf41ff94a5b9949dc7feffc3
nora0311 <664093036@qq.com>
2022-04-03 21:44:16 +0800

    demo 用例

---
+++ test_jenkins/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/4/3 9:33 PM
+# @Author  : 两只猫的铲屎官
+# @File    : __init__.py.py
---
+++ test_jenkins/test_ceshiren.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/4/3 9:33 PM
+# @Author  : 两只猫的铲屎官
+# @File    : test_ceshiren.py
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+
+
+class TestCeshiren:
+
+    def setup(self):
+        self.driver=webdriver.Chrome()
+        self.driver.get("https://ceshiren.com")
+        self.driver.implicitly_wait(30)
+
+    def test_search(self):
+        self.driver.find_element(By.ID,'search-button').click()
+        self.driver.find_element(By.ID,'search-term').send_keys("测试"+Keys.ENTER)
+        self.driver.find_element(By.CSS_SELECTOR,'[title="打开高级搜索"]').click()
