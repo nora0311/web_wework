6412b6bad9a8e5d8ed4047df593d365116dfcf66
nora0311 <664093036@qq.com>
2022-04-04 01:37:12 +0800

    多浏览器

--- test_jenkins/test_ceshiren.py
+++ test_jenkins/test_ceshiren.py
@@ -2,17 +2,26 @@
 # @Time    : 2022/4/3 9:33 PM
 # @Author  : 两只猫的铲屎官
 # @File    : test_ceshiren.py
+import os
+
 from selenium import webdriver
-from selenium.webdriver import Keys
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 
 class TestCeshiren:
 
     def setup_class(self):
-        self.driver=webdriver.Chrome()
+        browser = os.getenv("env")
+        if browser == "firefox":
+            self.driver = webdriver.Firefox()
+        elif browser == "edge":
+            self.driver = webdriver.Edge()
+        else:
+            self.driver = webdriver.Chrome()
+
         self.driver.get("https://ceshiren.com")
-        self.driver.implicitly_wait(30)
+        self.driver.implicitly_wait(10)
 
     def test_search(self):
         self.driver.find_element(By.ID,'search-button').click()
